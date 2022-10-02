c95dc81444bcf0df5d1be8dc586f80c12cb13800
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-02 21:51:46 +0530

    installed  mui core + icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Navbar.css";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Navbar() {
   return (
     <div className="navbar">
       <h2>Nav bar</h2>
+
+      <div className="navbar_top-area">
+        <h3>Oldest Home</h3>
+        <ExpandMoreIcon />
+      </div>
     </div>
   );
 }
