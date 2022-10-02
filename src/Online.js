a0222e7f5258791d8a1afc00602a5e0a6703f671
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-03 03:45:59 +0530

    adding placeholder online area

---
+++ src/Online.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Online.css";
+
+function Online() {
+  return (
+    <div className="online">
+      <h2>Placeholder Text</h2>
+    </div>
+  );
+}
+
+export default Online;
