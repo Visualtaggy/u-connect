4ab5f776daa010834df5bd093ae2106345d1b830
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-03 04:28:51 +0530

    added chat header

---
+++ src/ChatHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ChatHeader.css";
+
+function ChatHeader() {
+  return (
+    <div className="chatheader">
+      <h3>General</h3>
+      <div className="pipe">|</div>
+      <h4>
+        Everyone should be familiar with this world,It's the one that always
+        appeared on Control's main-menu, blurred and slowly rotating around.
+      </h4>
+    </div>
+  );
+}
+
+export default ChatHeader;
