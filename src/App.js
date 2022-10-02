0f1a0d561d414c12ea4742a4b4ebaff552427684
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-02 21:04:34 +0530

    adding Navbar to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="app">
       <h1>U-Connect</h1>
+
+      {/* nav bar area*/}
+      <Navbar />
     </div>
   );
 }
