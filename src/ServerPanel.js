c22fba8537fabcfd1bb173e829840673cf1e7ae6
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-03 16:40:41 +0530

    adding profile

--- src/ServerPanel.js
+++ src/ServerPanel.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./ServerPanel.css";
 import { Avatar } from "@material-ui/core";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import GroupRoundedIcon from "@mui/icons-material/GroupRounded";
 
 function ServerPanel() {
   return (
     <div className="serverPanel">
       <div className="server_profile">
-        <Avatar />
+        <Avatar src="https://avatars.githubusercontent.com/u/57157497?v=4" />
         <div className="server_profileinfo">
           <h3>visualtaggy</h3>
           <p>#1011</p>
         </div>
+      </div>
+
+      <div className="server_settings-icons">
+        <SettingsRoundedIcon />
+        <NotificationsRoundedIcon />
+        <GroupRoundedIcon />
       </div>
     </div>
   );
