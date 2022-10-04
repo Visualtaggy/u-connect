d806a6d5551bc2398ef632c47e0664d0a58576f2
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-04 17:10:02 +0530

    added code for dummy message

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Message.css";
+import { Avatar } from "@material-ui/core";
 function Message() {
-  return <div className="message"></div>;
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="message-details">
+        <h4>
+          visualtaggy
+          <span className="timestamp">11:30pm</span>
+        </h4>
+        <p>This is a dummy message !</p>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
