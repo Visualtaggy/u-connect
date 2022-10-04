26cc75b7db12752911fc59e96713398b5d86ec96
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-04 17:51:11 +0530

    adding firebase intigration

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYI33frPEPQ72xhMj2COxA99Zui_aYZwY",
+  authDomain: "u-connect-5624f.firebaseapp.com",
+  projectId: "u-connect-5624f",
+  storageBucket: "u-connect-5624f.appspot.com",
+  messagingSenderId: "251802049343",
+  appId: "1:251802049343:web:304941e484340a96bd118a",
+  measurementId: "G-K32P49QDE1",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
