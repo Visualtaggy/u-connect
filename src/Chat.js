7e5b49380514edf56cd73b268c2553f3247cbd3d
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-04 16:57:24 +0530

    calling message component

--- src/Chat.js
+++ src/Chat.js
@@ -3,12 +3,15 @@ import "./Chat.css";
 import ChatHeader from "./ChatHeader";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import Message from "./Message";
 
 function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
-      <div className="chat__messages"></div>
+      <div className="chat__messages">
+        <Message />
+      </div>
       <div className="chat_input">
         <AddCircleIcon fontSize="large" />
         <form action="">
