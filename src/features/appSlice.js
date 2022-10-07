66472439a08fc034986ce8891427e783b54cd8aa
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-07 10:41:51 +0530

    states for u-connect channel id and channel name

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -3,21 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const appSlice = createSlice({
   name: "app",
   initialState: {
-    app: null,
+    channelID: null,
+    channelName: null,
   },
 
   reducers: {
-    login: (state, action) => {
+    setChannelID: (state, action) => {
       state.app += action.payload;
-    },
-    logout: (state) => {
-      state.app = null;
     },
   },
 });
 
-export const { login, logout } = appSlice.actions;
+export const { setChannelID } = appSlice.actions;
 
-export const selectUser = (state) => state.app.app;
+export const selectChannelID = (state) => state.app.channelID;
+export const selectChannelName = (state) => state.app.channelName;
 
 export default appSlice.reducer;
