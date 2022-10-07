d9f5d18450caf8d8dfa3a54be9f870dc9d7519ba
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-07 10:37:46 +0530

    adding app slice

---
+++ src/features/appSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    app: null,
+  },
+
+  reducers: {
+    login: (state, action) => {
+      state.app += action.payload;
+    },
+    logout: (state) => {
+      state.app = null;
+    },
+  },
+});
+
+export const { login, logout } = appSlice.actions;
+
+export const selectUser = (state) => state.app.app;
+
+export default appSlice.reducer;
