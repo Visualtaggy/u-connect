e2bdaed678b77429997602e756e2b87888a8bffe
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-07 18:21:55 +0530

    adding code for html

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,72 @@
 import React from "react";
 import "./Login.css";
-
+import starImg from "./imgs/stars.png";
+import moonImg from "./imgs/moon.png";
+import midImg from "./imgs/mid.png";
+import land from "./imgs/lower.png";
 function Login() {
   return (
     <div>
-      <h1>TEST</h1>
+      <header>
+        <a href="#" className="logo">
+          Jinx
+        </a>
+
+        <ul>
+          <li className="active">Home</li>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+
+      <section>
+        <img src={starImg} id="stars" />
+        <img src={moonImg} id="moon" />
+        <img src={midImg} id="mid" />
+        <h2 id="text">Welcome To Jinx</h2>
+        <a href="#" className="Btn">
+          Login
+        </a>
+        <img src={land} id="land" />
+      </section>
+
+      <div className="sec">
+        <h2>Invite-Only Place</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non atque
+          inventore hic accusantium, repudiandae quod et dolorum itaque quasi
+          porro beatae? Placeat quisquam voluptates magni unde mollitia
+          obcaecati, minus voluptate, dolorem sunt est iste ad odio aliquid
+          ipsum velit veniam.
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+          molestiae consequatur consequuntur vero voluptates itaque cupiditate
+          aliquid atque laborum ipsam facere perspiciatis, magnam veniam rerum,
+          iste eaque doloremque a et est reprehenderit animi! Nam, consectetur
+          ducimus. Recusandae dignissimos, iste voluptate architecto nam harum
+          facilis, totam esse at non, maxime labore!
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+          ipsam repudiandae iste at et eaque, excepturi vel voluptate porro
+          mollitia iusto cupiditate? Debitis maiores beatae neque ipsa molestiae
+          provident, ad quae in vel vitae possimus deserunt! Ea magni, velit id
+          modi tempora ut similique dignissimos itaque numquam quos unde nihil
+          animi voluptate? Pariatur cum, officiis molestias obcaecati fuga
+          itaque omnis. Inventore molestiae neque eligendi! Rerum repellendus,
+          velit exercitationem non eaque doloremque delectus omnis cupiditate.
+          Est voluptas fugiat, repudiandae obcaecati veniam ducimus, non
+          temporibus reiciendis minus pariatur officiis tempore nemo itaque
+          numquam. Excepturi suscipit consectetur quos exercitationem, optio
+          fugit itaque repellendus.
+        </p>
+      </div>
     </div>
   );
 }
