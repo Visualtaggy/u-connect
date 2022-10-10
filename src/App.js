696ccee7c90ccb2b22a5d1b6bffe47d1b30a55b7
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-10 11:43:37 +0530

    dispatching login logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Navbar from "./Navbar";
@@ -6,25 +6,32 @@ import Online from "./Online";
 import ServerPanel from "./ServerPanel";
 import Login from "./Login";
 
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, selectUser, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 
 function App() {
+  const dispath = useDispatch();
   const user = useSelector(selectUser);
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      if(authUser){
-       //Logged In User 
-      }
-      else{
+      console.log("current user is", authUser);
+      if (authUser) {
+        //Logged In User
+        dispath(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
         //Logged Out User
-
       }
-    })
-  }, 
-  [user]);
+    });
+  }, []);
 
   return (
     <div className="app">
