4d09ccb3b4e8a997c35dfe8c519e7fc971e9b461
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-13 17:34:43 +0530

    using dipatcher to update name of channel in RT

--- src/NavbarChannel.js
+++ src/NavbarChannel.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./NavbarChannel.css";
 import LanguageIcon from "@mui/icons-material/Language";
+import { useDispatch } from "react-redux";
+import { setChannelID, setChannelInfo } from "./features/appSlice";
 
 function NavbarChannel({ id, channelName }) {
+  const dispatch = useDispatch();
+
   return (
-    <div className="navbarChannel">
+    <div
+      className="navbarChannel"
+      onClick={() =>
+        dispatch(
+          setChannelInfo({
+            channelID: id,
+            channelName: channelName,
+          })
+        )
+      }
+    >
       <div className="channel-highlight">
         <LanguageIcon />
         <h4>{channelName}</h4>
