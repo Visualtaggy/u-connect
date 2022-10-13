e1139e3445db74c4d8af3b888b5adab59d899dbb
Vishal Tyagi <visualtaggy@gmail.com>
2022-10-13 18:15:30 +0530

    Message feature from firebase

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import ChatHeader from "./ChatHeader";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
@@ -7,21 +7,45 @@ import Message from "./Message";
 import { useSelector } from "react-redux";
 import { selectChannelID, selectChannelName } from "./features/appSlice";
 import { selectUser } from "./features/userSlice";
+import db from "./firebase";
+import firebase from "firebase/compat/app";
 
 function Chat() {
   const channelid = useSelector(selectChannelID);
   const user = useSelector(selectUser);
   const channelname = useSelector(selectChannelName);
-
+  const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    if (channelid) {
+      db.collection("channels")
+        .doc(channelid)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [channelid]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("channels").doc(channelid).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user,
+    });
+    setInput("");
+  };
 
   return (
     <div className="chat">
       <ChatHeader channelName={channelname} />
       <div className="chat__messages">
-        <Message />
-        <Message />
-        <Message />
+        {messages.map((message) => {
+          <Message />;
+        })}
       </div>
       <div className="chat_input">
         <AddCircleIcon fontSize="large" />
@@ -31,7 +55,11 @@ function Chat() {
             onChange={(e) => setInput(e.target.value)}
             placeholder={`Message #${channelname}`}
           />
-          <button className="chatinput_area" type="submit"></button>
+          <button
+            onClick={sendMessage}
+            className="chatinput_area"
+            type="submit"
+          ></button>
         </form>
 
         <div className="chatinput_icons">
